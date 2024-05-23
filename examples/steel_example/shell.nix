01eea09b8dfed490b40f05b4c7ef65d566a7d259
DashieTM <fabio.lenherr@gmail.com>
2024-05-23 16:57:38 +0200

    chore: Change color of all themed screenshots

---
+++ examples/steel_example/shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+mkShell
+{
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    dbus
+    gtk4
+    libadwaita
+    pulseaudio
+    xorg.setxkbmap
+    openssl
+  ];
+
+}
